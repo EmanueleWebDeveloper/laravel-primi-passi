9c81ff91a94cbfed4ae3bf12e1098c5dc03820a2
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-03-12 19:00:36 +0100

    hello world

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'name' => 'LARAVEL'
+    ]; 
+    return view('home', compact( 'data' ));
 });
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,19 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+    laravel({
+    input: [
+    // Modifichiamo il percorso del css usando sass
+    'resources/scss/app.scss',
+    'resources/js/app.js'
     ],
-});
+    refresh: true,
+    }),
+    ],
+    //Aggiungiamo un alias per la cartella /resources/
+    resolve: {
+    alias: {
+    '~resources': '/resources/'
+    }
+    },
+    });
