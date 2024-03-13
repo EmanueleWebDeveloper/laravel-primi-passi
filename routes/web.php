a35bb1c8478a0b83baa6c9bd4095fa31c40c02a4
EmanueleWebDeveloper <emanuelelucacali@gmail.com>
2024-03-13 15:05:52 +0100

    template

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
     $data = [
-        'name' => 'LARAVEL'
-    ]; 
-    return view('home', compact( 'data' ));
+        [
+            'name' => 'Fabio'
+        ],
+        [
+            'name' => 'Mario'
+        ],
+        [
+            'name' => 'Sara'
+        ],
+        [
+            'name' => 'Alex'
+        ],
+    ];
+
+    return view('welcome', compact('data'));
+});
+
+Route::get('/about', function () {
+
+    return view('about');
 });
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
-    laravel({
-    input: [
-    // Modifichiamo il percorso del css usando sass
-    'resources/scss/app.scss',
-    'resources/js/app.js'
+        laravel({
+            input: ["resources/scss/app.scss", "resources/js/app.js"],
+            refresh: true,
+        }),
     ],
-    refresh: true,
-    }),
-    ],
-    //Aggiungiamo un alias per la cartella /resources/
     resolve: {
-    alias: {
-    '~resources': '/resources/'
-    }
+        alias: {
+            "~resources": "/resources/",
+        },
     },
-    });
+});
